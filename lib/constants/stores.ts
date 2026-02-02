144205c60e31d0e26d5e60e9fde10a87dbc4d7bc
ye-gang <yegang13209@naver.com>
2026-02-02 16:29:24 +0900

    스토어 목록 오타 수정

--- lib/constants/stores.ts
+++ lib/constants/stores.ts
@@ -7,5 +7,5 @@ export const STORES: Store[] = [
   { id: "seoulvinyl", name: "서울 바이닐" },
   { id: "smartstore", name: "스마트스토어" },
   { id: "yes24", name: "예스24" },
-  { id: "instagram", name: "인스타" },
+  { id: "instagram", name: "인스타그램" },
 ]

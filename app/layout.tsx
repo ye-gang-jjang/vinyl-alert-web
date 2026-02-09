371bd88060b55523a946ff24c1870a684edb42db
ye-gang <yegang13209@naver.com>
2026-02-09 14:32:24 +0900

    Google Search Console 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export const metadata: Metadata = {
   title: "L Pick | 원하는 LP를 한눈에",
   description:
     "LP 발매·판매처 정보를 모아 내가 원하는 레코드를 빠르게 찾을 수 있는 서비스",
+  verification: {
+    google: "p8orPfs8ylwY0IyA2L_rFz3uvyp7UDZCYdUl9Mjj-rA"
+  },
   openGraph: {
     title: "L Pick",
     description: "LP 발매·판매처 정보를 한눈에. 내가 원하는 LP를 픽하다.",
